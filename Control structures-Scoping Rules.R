fd6e88eb96e0179adc030e92b1a1f27dfe1b1b2f
phase-plane <alexander.johannes.17@ucl.ac.uk>
2021-05-27 14:53:30 +0200

    added examples from lecture videos

--- Control structures-Scoping Rules.R
+++ Control structures-Scoping Rules.R
@@ -64,4 +64,54 @@ NULL
 
 # Scoping Rules
 
-# lexical scoping (like python)
+# 1. Symbol Binding
+
+# first searches Global Environment (Workspace)
+# then all packages loaded in R
+# displayed using search()
+# base package last
+# order matters
+# seperate namespaces for functions and non-functions i.e. common symbol
+
+# Lexical scoping (like python)
+
+# e.g.s
+f <- function(x,y){
+  x^2 + y / z
+}
+
+# z is a free variable 
+# lexical scoping > how values of free variables are determined
+
+make.power <- function(n){
+  pow <- function(x){
+    x^n
+  }
+  pow
+}
+# n is a free variable
+# can take different values in the functions we construct
+
+y <- 10
+
+f <- function(x){
+  y <- 2
+  y^2 + g(x)
+}
+
+g <- function(x){
+  x*y
+}
+
+# lexical scoping
+# > f(3)
+# [1] 34
+# inside g, y is a free variable > looked up in environment in which
+# it was defined > global enviornment > y <- 10
+
+# dynamic scoping
+# y looked up in environment in which the function was called (parent frame) [f]
+# y <- 2
+
+# useful application > optimisation 
+# optim(), nlm(), optimise()

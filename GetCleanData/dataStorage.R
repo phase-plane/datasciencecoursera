9c3d83f08e38dee4f4a7128fa0eca79016a6cf0d
phase-plane <alexander.johannes.17@ucl.ac.uk>
2021-07-05 12:35:14 +0200

    End of week 2 lecture videos.

--- GetCleanData/dataStorage.R
+++ GetCleanData/dataStorage.R
@@ -1,2 +1,166 @@
 setwd(paste(getwd(),"GetCleanData", sep="/"))
 # Week 2: Getting Data from common data storage systems
+
+# (i) MySQL
+
+# data structured in:
+# (a) databases
+# (b) tables within databases
+# (c) fields within tables
+# each row is called a 'record'
+install.packages("RMySQL")
+library(RMySQL)
+
+# connect (using MySQL commands)
+ucscDb <- dbConnect(MySQL(),user="genome", 
+                    host="genome-euro-mysql.soe.ucsc.edu")
+result <- dbGetQuery(ucscDb,"show databases;"); dbDisconnect(ucscDb);
+
+# access specific database
+hg19 <- dbConnect(MySQL(),user="genome", db="hg19",
+                  host="genome-euro-mysql.soe.ucsc.edu")
+allTables <- dbListTables(hg19)
+length(allTables)
+allTables[1:5]
+
+# get dimensions of specific table / column names
+dbListFields(hg19,"affyU133Plus2")
+
+# how many rows
+dbGetQuery(hg19, "select count(*) from affyU133Plus2")
+
+# read the actual data from the table
+affyData <- dbReadTable(hg19, "affyU133Plus2")
+head(affyData)
+
+# select specific subset
+query <- dbSendQuery(hg19, "select * from affyU133Plus2 where
+                     misMatches between 1 and 3")
+affyMis <- fetch(query); quantile(affyMis$misMatches)
+
+# check the data
+affyMisSmall <- fetch(query,n=10); dbClearResult(query);
+dim(affyMisSmall)
+dbDisconnect(hg19)
+
+# (ii) HDF5
+
+install.packages("BiocManager")
+BiocManager::install(c("rhdf5"))
+library(rhdf5)
+
+# create our own file
+created = h5createFile("example.h5")
+
+# create groups within file
+created = h5createGroup("example.h5","foo")
+created = h5createGroup("example.h5","baa")
+created = h5createGroup("example.h5","foo/foobaa")
+
+# similar to ls() i.e. list command
+h5ls("example.h5")
+
+# write to groups
+A = matrix(1:10,nr=5,nc=2)
+h5write(A, "example.h5","foo/A")
+
+B = array(seq(0.1,2.0,by=0.1),dim=c(5,2,2))
+attr(B, "scale") <- "litre"
+h5write(B, "example.h5","foo/foobaa/B")
+
+# print
+h5ls("example.h5")
+
+# write a data set (to top level group)
+df = data.frame(1L:5L,seq(0,1,length.out=5),
+                c("ab","cde","fghi","a","s"), stringsAsFactors=FALSE)
+h5write(df, "example.h5","df")
+h5ls("example.h5")
+
+# reading data
+readA = h5read("example.h5","foo/A")
+readB = h5read("example.h5","foo/foobaa/B")
+readdf= h5read("example.h5","df")
+readA
+
+# read and write 'chunks'
+h5write(c(12,13,14),"example.h5","foo/A",index=list(1:3,1))
+h5read("example.h5","foo/A")
+
+# could pass index=list(1:3,1) to h5read() as well
+
+# (iii) The WEB / web scraping (see rbloggers)
+
+# webpages
+# con -> connection
+con = url("http://scholar.google.com/citations?user=HI-I6C0AAAAJ&hl=en")
+htmlCode = readLines(con)
+close(con)
+htmlCode
+
+# parsing with XML
+library(XML)
+url <- "http://scholar.google.com/citations?user=HI-I6C0AAAAJ&hl=en"
+html <- htmlTreeParse(url, useInternalNodes=TRUE)
+
+xpathSApply(html, "//title", xmlValue)
+xpathSApply(html, "//td[@id='col-citedby']", xmlValue)
+
+# GET from httr package
+install.packages("httr")
+library(httr)
+
+html2 = GET(url)
+content2 = content(html2,as="text")
+parsedHtml = htmlParse(content2,asText=TRUE)
+xpathSApply(parsedHtml, "//title", xmlValue)
+
+# websites with passwords
+pg2 = GET("http://httpbin.org/basic-auth/user/passwd",
+          authenticate("user","passwd"))
+pg2
+names(pg2)
+
+# using handles
+google = handle("http://google.com")
+pg1 = GET(handle=google,path="/")
+pg2 = GET(handle=google,path="search")
+
+# (iv) APIs
+# using httr
+# access twitter API
+myapp = oauth_app("twitter",
+                  key="yourConsumerKeyHere",secret="yourConsumerSecretHere")
+sig = sign_oauth1.0(myapp,
+                    token = "yourTokenHere",
+                    token_secret = "yourTokenSecretHere")
+homeTL = GET("https://api.twitter.com/1.1/statuses/home_timeline.json", sig)
+# how to find the URL -> documentation of twitter API
+
+
+# convert the json object
+json1 = content(homeTL)
+json2 = jsonlite::fromJSON(toJSON(json1))
+json2[1,1:4]
+
+# (v) other Sources
+
+# interact directly with files
+file() # open a connection to a text file
+url() # open a connection to a URL
+gzfile()  # open a connection to a .gz file (zipped)
+bzfile() # open a connection to a .bz2 file
+?connections # for more information
+# remember to close connections
+
+# foreign package
+# loads data from Minitab, S, SAS, SPSS, Stata, Systat
+# e.g. 
+read.xport # (SAS)
+
+# other data base packages
+RPostresSQL()
+RODBC()
+RMongo()
+
+# can read images & gis data

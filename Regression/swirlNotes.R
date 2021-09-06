24cf657825df12ad243d2d7c18394850129994a3
phase-plane <alexander.johannes.17@ucl.ac.uk>
2021-09-06 20:27:27 +0200

    Updates post project completion.

--- Regression/swirlNotes.R
+++ Regression/swirlNotes.R
@@ -220,7 +220,7 @@ qchisq(.95,1)
 # lesson 13: Count Outcomes
 var(rpois(1000,50))
 # In a Poisson regression, the log of lambda is assumed to be a linear function of
-# the predictors.
+# the predictors
 class(hits[,'date'])
 as.integer(head(hits[,'date']))
 mdl <- glm(visits ~ date, family = poisson, data = hits)
@@ -233,3 +233,61 @@ qpois(.95, lambda)
 mdl2 <- glm(simplystats ~ date, family = poisson, data = hits,
             offset = log(visits+1))
 qpois(.95, mdl2$fitted.values[704])
+
+# Quiz 1
+# Q1
+x <- c(0.18, -1.54, 0.42, 0.95)
+mean(x)
+
+# Q2
+x <- c(0.8, 0.47, 0.51, 0.73, 0.36, 0.58, 0.57, 0.85, 0.44, 0.42)
+y <- c(1.39, 0.72, 1.55, 0.48, 1.19, -1.59, 1.23, -0.65, 1.49, 0.05)
+mydata <- data.frame(y,x)
+fit1 <- lm(y~x, mydata)
+summary(fit1)
+
+# Q3
+data("mtcars")
+mtcars$cyl <- factor(mtcars$cyl)
+fit1 <- lm(mpg ~ cyl + wt, mtcars)
+fit2 <- lm(mpg ~ cyl, mtcars)
+summary(fit1)$coef[3,1]
+summary(fit2)$coef[3,1]
+
+
+# Quiz 2
+x <- c(0.61, 0.93, 0.83, 0.35, 0.54, 0.16, 0.91, 0.62, 0.62)
+y <- c(0.67, 0.84, 0.6, 0.18, 0.85, 0.47, 1.1, 0.65, 0.36)
+mydata <- data.frame(y,x)
+fit1 <- lm(y~x, mydata)
+
+fit_interaction <- lm(mpg ~ cyl + wt + cyl:wt, mtcars)
+lm(mpg ~ I(wt * 0.5) + factor(cyl), data = mtcars)
+
+# Quiz 4
+install.packages("MASS")
+library(MASS)
+data("shuttle")
+library(dplyr)
+as.integer(tail(shuttle[,7], 30))
+shuttle <- mutate(shuttle, use = relevel(use, ref = "noauto"))
+shuttle$use.bin <- as.integer(shuttle$use) -1 
+mymdl <- glm(use.bin ~ wind - 1, family = 'binomial', data = shuttle)
+summary(mymdl)
+exp(coef(mymdl))
+exp(coef(mymdl))[1] / exp(coef(mymdl))[2] 
+
+mymdl2 <- glm(use.bin ~ wind + magn - 1, family = 'binomial', data = shuttle)
+exp(coef(mymdl2))[1] / exp(coef(mymdl2))[2] 
+
+x <- -5:5
+y <- c(5.12, 3.93, 2.67, 1.87, 0.52, 0.08, 0.93, 2.05, 2.54, 3.87, 4.97)
+
+plot(x, y, pch = 21,  cex = 2, col="grey20", bg="cadetblue2")
+
+knots <- 0
+splineTerms <- sapply(knots, function(knot) (x > knot) * (x - knot))
+xmat <- cbind(1, x, splineTerms)
+mdl6 <- lm(y~xmat-1)
+yhat<-predict(mdl6)
+lines(x, yhat, col = "red", lwd = 2)
